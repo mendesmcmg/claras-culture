9853a90ba05103e6482b04f53f81dada9648de90
Clara Mendes <claraplanejamento@gmail.com>
2022-03-02 19:24:52 -0300

    fix: runs prettier

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,11 @@ import "./Home.css";
 
 function Home() {
   const [topics, setTopics] = useState([]);
-  const imageUrls = ["https://somos.lojaiplace.com.br/wp-content/uploads/2020/01/cinema-CRED-iStock_Roman-Valiev.jpg", "https://i.pinimg.com/564x/6e/1c/ff/6e1cff24cdbe4d7cf6de3c7730fbedfb.jpg", "https://i.pinimg.com/564x/03/51/4b/03514b2aead177c5c1c086c94080ae3d.jpg"]
+  const imageUrls = [
+    "https://somos.lojaiplace.com.br/wp-content/uploads/2020/01/cinema-CRED-iStock_Roman-Valiev.jpg",
+    "https://i.pinimg.com/564x/6e/1c/ff/6e1cff24cdbe4d7cf6de3c7730fbedfb.jpg",
+    "https://i.pinimg.com/564x/03/51/4b/03514b2aead177c5c1c086c94080ae3d.jpg",
+  ];
 
   useEffect(() => {
     getTopics().then((topics) => setTopics(topics));
@@ -23,7 +27,7 @@ function Home() {
               <Card
                 key={topic.id}
                 title={topic.title}
-                image={imageUrls[topic.id-1]}
+                image={imageUrls[topic.id - 1]}
                 description={topic.title}
               />
             ))
