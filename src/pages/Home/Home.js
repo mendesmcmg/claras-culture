1f054f299e4a438229614937c0315e4b04df59ab
Clara Mendes <claraplanejamento@gmail.com>
2022-03-02 19:04:06 -0300

    feat: adds dynamic fetching of topics

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import "./Home.css";
 
 function Home() {
   const [topics, setTopics] = useState([]);
+  const imageUrls = ["https://somos.lojaiplace.com.br/wp-content/uploads/2020/01/cinema-CRED-iStock_Roman-Valiev.jpg", "https://i.pinimg.com/564x/6e/1c/ff/6e1cff24cdbe4d7cf6de3c7730fbedfb.jpg", "https://i.pinimg.com/564x/03/51/4b/03514b2aead177c5c1c086c94080ae3d.jpg"]
 
   useEffect(() => {
     getTopics().then((topics) => setTopics(topics));
@@ -17,23 +18,16 @@ function Home() {
       </div>
 
       <div className="content-container">
-        <Card
-          title="Filmes"
-          image="https://somos.lojaiplace.com.br/wp-content/uploads/2020/01/cinema-CRED-iStock_Roman-Valiev.jpg"
-          description="movie theater"
-        />
-
-        <Card
-          title="Livros"
-          image="https://i.pinimg.com/564x/6e/1c/ff/6e1cff24cdbe4d7cf6de3c7730fbedfb.jpg"
-          description="books"
-        />
-
-        <Card
-          title="Computação"
-          image="https://i.pinimg.com/564x/03/51/4b/03514b2aead177c5c1c086c94080ae3d.jpg"
-          description="computer"
-        />
+        {topics
+          ? topics.map((topic) => (
+              <Card
+                key={topic.id}
+                title={topic.title}
+                image={imageUrls[topic.id-1]}
+                description={topic.title}
+              />
+            ))
+          : null}
       </div>
     </div>
   );
