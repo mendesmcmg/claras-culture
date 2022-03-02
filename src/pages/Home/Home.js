3e7daac918a905daae20da5088083e1a69d24dc4
Clara Mendes <claraplanejamento@gmail.com>
2022-03-02 15:25:05 -0300

    feat: adds home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="title-container">
+        <h1>Clara's Culture</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
